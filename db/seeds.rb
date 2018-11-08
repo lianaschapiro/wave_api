3a50cdd3e95db520ba3d56bce57579c71d6d867b
lianaschapiro <liana.schapiro@gmail.com>
2018-11-08 18:45:35 -0500

    add seed data for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 5.times do
+#   Exchange.create({
+#   })
+# end
+
+# Payin.create([{ exchange_id: 1, amount: 121.13451, timestamp: '2017-01-05 12:00:12'}])
+# Payin.create([{ exchange_id: 1, amount: 123.2345353, timestamp: '2018-01-01 11:59:59'}])
+# Payin.create([{ exchange_id: 2, amount: 111.1134435, timestamp: '2018-01-01 12:00:00'}])
+# Payin.create([{ exchange_id: 3, amount: 144.443454, timestamp: '2018-01-01 12:00:01'}])
+# Payin.create([{ exchange_id: 4, amount: 139.0434354, timestamp: '2018-01-01 12:01:12'}])
+# Payin.create([{ exchange_id: 5, amount: 121.113455, timestamp: '2018-01-05 12:00:12'}])
+
+# Payout.create([{ exchange_id: 1, amount: 122.23, timestamp: '2018-01-01 12:00:59'}])
+# Payout.create([{ exchange_id: 2, amount: 110.11, timestamp: '2018-01-01 12:01:00'}])
+# Payout.create([{ exchange_id: 3, amount: 143.44, timestamp: '2018-01-01 12:01:01'}])
+# Payout.create([{ exchange_id: 4, amount: 138.04, timestamp: '2018-01-04 12:00:12'}])
+# Payout.create([{ exchange_id: 5, amount: 120.11, timestamp: '2018-01-05 12:00:12'}])
+# Payout.create([{ exchange_id: 1, amount: 122.23, timestamp: '2018-01-01 12:00:59'}])
+# Payout.create([{ exchange_id: 2, amount: 110.11, timestamp: '2018-01-01 12:01:12'}])
+# Payout.create([{ exchange_id: 3, amount: 143.44, timestamp: '2018-01-01 12:01:01'}])
